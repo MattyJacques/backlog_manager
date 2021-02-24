6e081dee68c3eece0a0d511e9da978f62b0333b1
Matthew Jacques <iFtw@live.co.uk>
2021-02-24 15:36:57 +0000

    Generate genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_200920) do
+ActiveRecord::Schema.define(version: 2021_02_24_151320) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "platform_families", force: :cascade do |t|
     t.string "name", null: false
