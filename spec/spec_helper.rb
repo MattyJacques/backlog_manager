3a743efcf6d9e8ddf26de1cf4fbcf357bf306aed
Matthew Jacques <iFtw@live.co.uk>
2023-05-26 08:38:45 +0100

    Fix Layout/LeadingCommentSpace offences

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,11 +30,11 @@ SimpleCov.start do
   add_filter %w[app/jobs app/workers]
 
   add_group "Controllers", "app/controllers"
-  #add_group "Channels", "app/channels"
+  # add_group "Channels", "app/channels"
   add_group "Models", "app/models"
-  #add_group "Mailers", "app/mailers"
+  # add_group "Mailers", "app/mailers"
   add_group "Helpers", "app/helpers"
-  #add_group "Jobs", %w[app/jobs app/workers]
+  # add_group "Jobs", %w[app/jobs app/workers]
   add_group "Libraries", "lib"
 
   track_files "{app,lib}/**/*.rb"
