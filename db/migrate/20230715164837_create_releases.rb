844e87a928c6d2605477920ff49789c071c6b3d6
Matthew Jacques <iFtw@live.co.uk>
2023-07-20 19:16:41 +0100

    Random fixes

--- db/migrate/20230715164837_create_releases.rb
+++ db/migrate/20230715164837_create_releases.rb
@@ -3,7 +3,7 @@ class CreateReleases < ActiveRecord::Migration[7.0]
     create_table :releases do |t|
       t.belongs_to :game, null: false, foreign_key: true
       t.belongs_to :platform, null: false, foreign_key: true
-      t.integer :region, null: false
+      t.integer :region
       t.date :date
       t.belongs_to :trophy_list, foreign_key: true, index: { unique: true, name: 'unique release trophy list index' }
 
