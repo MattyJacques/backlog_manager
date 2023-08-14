5399d5b0965886c2e217ab52adc53d919463e116
Matthew Jacques <iFtw@live.co.uk>
2023-08-14 16:15:37 +0100

    Fix bootstrap dropdown (#10)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails'
-import "@popperjs/core";
-import "bootstrap";
-import 'controllers';
-
-$(document).on('turbolinks:load', function() {
-  $('.dropdown-toggle').dropdown()
-})
-
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import '@popperjs/core'
+import 'bootstrap'
+import 'controllers'
