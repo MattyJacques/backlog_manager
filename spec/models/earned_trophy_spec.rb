cd3c033db7a192767644f07eb90cf3212509fd4a
Matthew Jacques <iFtw@live.co.uk>
2023-08-21 11:41:36 +0100

    Add in missed association specs

--- spec/models/earned_trophy_spec.rb
+++ spec/models/earned_trophy_spec.rb
@@ -5,4 +5,5 @@ require 'rails_helper'
 RSpec.describe EarnedTrophy do
   it { is_expected.to belong_to(:psn_account) }
   it { is_expected.to belong_to(:trophy) }
+  it { is_expected.to belong_to(:trophy_list) }
 end
